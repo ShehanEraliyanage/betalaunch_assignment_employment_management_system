3a7e083fce90af2f8c5926afbccb40fcb911acfe
Shehan <34shehan@gmail.com>
2023-05-31 17:24:50 +0530

    chnage

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ import employeeRoutes from "./routes/employee.js";
 // Constants
 dotenv.config();
 const URL = process.env.MONGODB_URL;
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 const app = express();
 
 app.use(cors());
