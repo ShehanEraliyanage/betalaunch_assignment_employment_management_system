678ef4d1c41813c172945918eb01316d0ce6314c
Shehan <34shehan@gmail.com>
2023-06-03 09:39:58 +0530

    add responsive

--- frontend/src/components/EmployeeView.js
+++ frontend/src/components/EmployeeView.js
@@ -77,87 +77,87 @@ export default function EmployeeView() {
   return (
     <>
       <div>
-        <h1 className=" mx-4 mt-4 text-lg font-bold border-b-2 pb-3">People</h1>
+        <h1 className="mx-4 mt-4 text-lg font-bold border-b-2 pb-3">People</h1>
       </div>
-      <div>
-        <div className="flex justify-end mr-10 -mb-4 mt-3">
-          <div className="mr-5">
-            <Select
-              options={employeeTypes}
-              value={selectedType}
-              onChange={handleTypeChange}
-              placeholder="Select employee type"
-            />
-          </div>
-          <div className=" bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      <div className="flex flex-col md:flex-row md:justify-between items-center mt-3 mx-4">
+        <div className="mb-3 md:mb-0 md:mr-5">
+          <Select
+            options={employeeTypes}
+            value={selectedType}
+            onChange={handleTypeChange}
+            placeholder="Select employee type"
+          />
+        </div>
+        <div className="flex justify-center mb-3 md:mb-0">
+          <div className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
             <Link to={"/Add"}>
               <button>Add People</button>
             </Link>
           </div>
         </div>
-        <div>
-          <div className="relative overflow-x-auto shadow-md sm:rounded-lg flex justify-center">
-            <table className="w-full m-10  text-sm text-left text-gray-500 dark:text-gray-400 border-2">
-              <thead className="text-xs text-gray-700 uppercase dark:text-gray-400 ">
-                <tr className="border-1">
-                  <th scope="col" className="py-3">
-                    <div className="flex items-center">Display name</div>
-                  </th>
-                  <th scope="col" className=" py-3">
-                    <div className="flex items-center">Emp ID</div>
-                  </th>
-                  <th scope="col" className=" py-3">
-                    <div className="flex items-center">Designation</div>
-                  </th>
-                  <th scope="col" className=" py-3">
-                    <div className="flex items-center">emp Type</div>
-                  </th>
-                  <th scope="col" className=" py-3">
-                    <div className="flex items-center">Experience</div>
-                  </th>
-                  <th scope="col">
-                    <div className="flex items-start"></div>
-                  </th>
-                  <th scope="col">
-                    <div className="flex items-start"></div>
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                {employees.map((value, index) => {
-                  return (
-                    <tr className="text-black border-2 " key={index}>
-                      <td className=" py-4 font-bold ">{value.displayName}</td>
-                      <td className=" py-3">{value.employeId}</td>
-                      <td className=" py-3">{value.designation}</td>
-                      <td className=" py-3">{value.eType}</td>
-                      <td className=" py-3">{value.experience}</td>
-                      <Link to={"/update/" + value._id}>
-                        <td className="text-eCol pt-4">Edit</td>
-                      </Link>
-                      <td className="text-dCol">
-                        <button onClick={() => deleteHandler(value._id)}>
-                          Delete
-                        </button>
-                      </td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
-          </div>
-          <div>
-            {" "}
-            <ReactPaginate
-              className="flex flex-row p-3 gap-x-2"
-              breakLabel={"..."}
-              pageCount={totalPages}
-              onPageChange={handlePageClick}
-              nextLabel="next >"
-              previousLabel="< previous"
-            />
-          </div>
-        </div>
+      </div>
+      <div className="overflow-x-auto shadow-md sm:rounded-lg mx-4 mt-10">
+        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400 border-2">
+          <thead className="text-xs text-gray-700 uppercase dark:text-gray-400">
+            <tr className="border-1">
+              <th scope="col" className="py-3">
+                <div className="flex items-center">Display name</div>
+              </th>
+              <th scope="col" className="py-3">
+                <div className="flex items-center">Emp ID</div>
+              </th>
+              <th scope="col" className="py-3">
+                <div className="flex items-center">Designation</div>
+              </th>
+              <th scope="col" className="py-3">
+                <div className="flex items-center">emp Type</div>
+              </th>
+              <th scope="col" className="py-3">
+                <div className="flex items-center">Experience</div>
+              </th>
+              <th scope="col">
+                <div className="flex items-start"></div>
+              </th>
+              <th scope="col">
+                <div className="flex items-start"></div>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {employees.map((value, index) => (
+              <tr className="text-black border-2" key={index}>
+                <td className="py-4 font-bold">{value.displayName}</td>
+                <td className="py-3">{value.employeId}</td>
+                <td className="py-3">{value.designation}</td>
+                <td className="py-3">{value.eType}</td>
+                <td className="py-3">{value.experience}</td>
+                <Link to={"/update/" + value._id}>
+                  <td className="text-eCol pt-4">Edit</td>
+                </Link>
+                <td className="text-dCol">
+                  <button onClick={() => deleteHandler(value._id)}>
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <div className="flex justify-center mt-5">
+        <ReactPaginate
+          containerClassName="flex flex-row p-3 gap-x-2"
+          pageClassName="px-3 py-2 text-gray-700 dark:text-gray-400 border border-gray-300 dark:border-gray-700 rounded-md cursor-pointer"
+          previousClassName="px-3 py-2 text-gray-700 dark:text-gray-400 border border-gray-300 dark:border-gray-700 rounded-md cursor-pointer"
+          nextClassName="px-3 py-2 text-gray-700 dark:text-gray-400 border border-gray-300 dark:border-gray-700 rounded-md cursor-pointer"
+          activeClassName="bg-blue-500 text-white"
+          breakClassName="px-3 py-2 text-gray-700 dark:text-gray-400 border border-gray-300 dark:border-gray-700 rounded-md cursor-pointer"
+          breakLinkClassName="px-3 py-2 text-gray-700 dark:text-gray-400 border border-gray-300 dark:border-gray-700 rounded-md cursor-pointer"
+          pageCount={totalPages}
+          onPageChange={handlePageClick}
+          previousLabel="< previous"
+          nextLabel="next >"
+        />
       </div>
     </>
   );
